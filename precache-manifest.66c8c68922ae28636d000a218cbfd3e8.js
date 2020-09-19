e805ca03937605c0e54d8e00b728da191db56fd7
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-19 12:00:43 +0200

    Updates

---
+++ precache-manifest.66c8c68922ae28636d000a218cbfd3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ab0ebf435f80f2b01256aed0c459cd",
+    "url": "/Pathfinder/index.html"
+  },
+  {
+    "revision": "828d4f1599e540a439cd",
+    "url": "/Pathfinder/static/css/main.cd7b3d9f.chunk.css"
+  },
+  {
+    "revision": "367b9a1f67910cd1926e",
+    "url": "/Pathfinder/static/js/2.aa153ead.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pathfinder/static/js/2.aa153ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "828d4f1599e540a439cd",
+    "url": "/Pathfinder/static/js/main.fd85176d.chunk.js"
+  },
+  {
+    "revision": "5edf268ae9bf78a53d6a",
+    "url": "/Pathfinder/static/js/runtime-main.a6f85a6b.js"
+  }
+]);
